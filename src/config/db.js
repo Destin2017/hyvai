63da0af166f53668267948bb9738aef808d3ff3f
Destin2017 <touskaki@gmail.com>
2025-05-19 19:23:00 +0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -5,13 +5,14 @@ dotenv.config();
 
 // Create a MySQL connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '',
-  user: process.env.DB_USER || '',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || '',
+  host: process.env.DB_HOST || 'hyvai-db.cj6ae26q6fc4.eu-north-1.rds.amazonaws.com',
+  user: process.env.DB_USER || 'admin',
+  password: process.env.DB_PASS || 'StrongPass#2025',
+  database: process.env.DB_NAME || 'employee_installment_db',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
 module.exports = pool;
+
